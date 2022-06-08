43ffbc092c16cc67b3e0235de4c11e66c990dfca
zaykhere <zainjhere@gmail.com>
2022-06-08 10:31:48 +0500

    added plugin code

---
+++ js/adapt-contrib-slider.js
@@ -0,0 +1,8 @@
+import components from 'core/js/components';
+import SliderView from './sliderView';
+import SliderModel from './sliderModel';
+
+export default components.register('slider', {
+  view: SliderView,
+  model: SliderModel
+});
